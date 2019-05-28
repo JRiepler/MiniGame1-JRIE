6c1f28d3b4981cff26993f0b0a1d0c1a3d742849
Jonas Riepler <jonas.riepler@htl-salzburg.ac.at>
2019-05-28 10:08:56 +0200

    Basic Truck Movement and Score, SceneLoader unknown Error

---
+++ Assets/Scripts/Collectible.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Collectible : MonoBehaviour
+{
+    private int score = 0;
+    public TextMeshProUGUI pointsScore;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Triggerevent " + collision.gameObject.name);
+
+        if (collision.gameObject.name == "Truck")
+        {
+            score += 10;
+            Debug.Log("score " + score);
+            pointsScore.text = score.ToString();
+
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("CollisionEvent");
+
+    }
+}
